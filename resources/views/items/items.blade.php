703ca460359f0cef94a260b198f652e0ad339c30
foundnation12 <foundnation12@ez.web.ne.jp>
2018-05-28 14:00:26 +0000

    ranking syuusei

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
@@ -23,7 +23,17 @@
                                 @if (Auth::check())
                                     @include('items.have_button', ['item' => $item])
                                 @endif
-                        </div>
+                        </div
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} {{$type}}</p>
+                            </div>
+                        @endif
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} {{$type}}</p>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
